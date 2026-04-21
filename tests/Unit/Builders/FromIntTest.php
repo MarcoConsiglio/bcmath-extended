b0ea23f9895e029faa65838ce6941fdca2421112
Marco Consiglio <mrccnsgl@gmail.com>
2026-04-21 10:29:42 +0000

    test: cover `FromInt` class

---
+++ tests/Unit/Builders/FromIntTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace MarcoConsiglio\BCMathExtended\Tests\Unit\Builders;
+
+use BcMath\Number;
+use MarcoConsiglio\BCMathExtended\Builders\FromInt;
+use MarcoConsiglio\BCMathExtended\Tests\BaseTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(FromInt::class)]
+class FromIntTest extends BaseTestCase
+{
+    public function test_builder(): void
+    {
+        // Arrange
+        $int = $this->randomInteger();
+        $builder = new FromInt($int);
+        $expected = new Number($int);
+
+        // Act
+        $actual = $builder->getResult();
+
+        // Assert
+        $this->assertSame($expected->value, $builder->getResult()->value);
+    }
+}
